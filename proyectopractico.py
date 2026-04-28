7ce6bbffa2fb41540bfd9cf2eb70da58d08e818d
mariojo18 <mariosoluciona15@gmail.com>
2026-04-28 17:16:53 -0500

    Add EDA and part of model development

--- proyectopractico.py
+++ proyectopractico.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import numpy as np
-
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression,LogisticRegression
 path = 'https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DA0101EN-Coursera/medical_insurance_dataset.csv'
 
 #*Data ingestion
@@ -38,3 +40,19 @@ df['charges']=df['charges'].round(2)
 print(df['charges'].head(5))
 
 #*EDA
+sns.regplot(x="bmi", y="charges", data=df, line_kws={"color":"blue"})
+plt.ylim(0,)
+plt.show()
+sns.boxplot(x="smoker",y="charges",data=df)
+corr= df.select_dtypes(include=['int64','float']).corr()
+plt.figure(figsize=(8,6))
+sns.heatmap(corr,annot=True,cmap="coolwarm")
+plt.show()
+
+#*Model Development
+
+x=df[['charges']]
+y=df['smoker']
+lrcs=LogisticRegression()
+lrcs.fit(x,y)
+print(lrcs.score(x,y))
